c1cde15d38961915dba65a7d07389caec1db1cd2
Radosław Maksym Hołubowski <holubowskir@gmail.com>
2026-03-19 14:46:16 +0100

    Create make.bat

---
+++ MATH/FIELD/make.bat
@@ -0,0 +1,42 @@
+@echo off
+
+REM Create math field files
+
+type nul > arithmetic.rs
+type nul > elementary-number-theory.rs
+type nul > fractions-decimals-percentages.rs
+type nul > pre-algebra.rs
+type nul > elementary-geometry.rs
+type nul > measurement.rs
+type nul > ratios-and-proportions.rs
+type nul > elementary-probability.rs
+type nul > elementary-statistics.rs
+type nul > intermediate-algebra.rs
+
+REM Added explicitly
+type nul > linear-equations.rs
+type nul > quadratic-equations.rs
+
+type nul > intro-linear-algebra.rs
+type nul > intro-trigonometry.rs
+type nul > coordinate-geometry.rs
+type nul > functions-and-relations.rs
+type nul > intro-discrete-mathematics.rs
+type nul > advanced-probability.rs
+type nul > advanced-statistics.rs
+type nul > differential-calculus.rs
+type nul > integral-calculus.rs
+type nul > multivariable-calculus.rs
+type nul > differential-equations.rs
+type nul > abstract-algebra.rs
+type nul > advanced-number-theory.rs
+type nul > topology.rs
+type nul > real-analysis.rs
+type nul > complex-analysis.rs
+type nul > advanced-linear-algebra.rs
+type nul > mathematical-logic-and-set-theory.rs
+type nul > differential-geometry.rs
+type nul > mathematical-physics.rs
+
+echo All math files created!
+pause
