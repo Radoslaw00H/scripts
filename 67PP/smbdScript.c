f111264caedf768b04a02724956b6eee713b9b14
Radosław Maksym Hołubowski <holubowskir@gmail.com>
2026-03-29 20:13:49 +0200

    Create smbdScript.c

---
+++ 67PP/smbdScript.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void smbdUFW() {
+    // code
+}
+
+void smbdFILEIO() {
+    // code
+}
+
+void smbdSetup() {
+    // code
+}
+
+void smbdAutoconfig() {
+    // code
+}
+
+int main(void) {
+    // functions
+    return 0;
+}
